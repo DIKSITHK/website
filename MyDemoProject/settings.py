17dcfc5e6ea5776b82c4bdb6c77b718cb0782053
akshayanagaraj <48725830+akshayanagaraj@users.noreply.github.com>
2020-12-09 16:39:55 +0530

    Update settings.py

--- MyDemoProject/settings.py
+++ MyDemoProject/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'Templates')
 SECRET_KEY = 'g187lyt1xz(il2g(^(1i=d4magewzgxv5xs6+%f)c@gft41bhu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['post-hub-django.herokuapp.com']
 
